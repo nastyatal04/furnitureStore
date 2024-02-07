4c09df6412c2f1b085fe9e23b400ae8729bb9212
Вадим Якубовский <wadimaukubowskij@gmail.com>
2024-02-07 20:48:17 +0300

    base

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,4 @@
 // export const baseURL = 'http://localhost:8080'
-export const baseURL = "http://158.160.48.208:8080";
+export const baseURL = `http://${
+  process.env.BACKEND_HOST ? process.env.BACKEND_HOST : "localhost"
+}:8080`;
