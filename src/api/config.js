446c89056ce9be7a57038cb44a6ead9045752073
Вадим Якубовский <wadimaukubowskij@gmail.com>
2024-02-09 19:16:06 +0300

    var updated

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,2 @@
 // export const baseURL = 'http://localhost:8080'
-export const baseURL = `http://${
-  process.env.BACKEND_HOST ? process.env.BACKEND_HOST : "localhost"
-}:8080`;
+export const baseURL = `http://${process.env.REACT_APP_BACKEND_HOST}:8080`;
--- src/components/color/ColorAdd.jsx
+++ src/components/color/ColorAdd.jsx
@@ -1,43 +1,43 @@
-import React, { useEffect } from 'react'
-import { useInput } from '../../api/hooks/useInput'
-import "./ColorAdd.css"
-import { useAddColor } from '../../api/hooks/hooks-color/useAddColor'
+import React, { useEffect } from "react";
+import { useInput } from "../../api/hooks/useInput";
+import "./ColorAdd.css";
+import { useAddColor } from "../../api/hooks/hooks-color/useAddColor";
 
 export const ColorAdd = () => {
-    const [colorName, onColorNameChange] = useInput()
-    const [colorHex, onColorHexChange] = useInput()
-    const [color, error, isLoading, fetchData] = useAddColor()
+  const [colorName, onColorNameChange] = useInput();
+  const [colorHex, onColorHexChange] = useInput();
+  const [color, error, isLoading, fetchData] = useAddColor();
 
-    useEffect(() => {
-        console.log(color);
-    }, [color])
+  useEffect(() => {
+    console.log(color);
+  }, [color]);
 
-
-    return (
-        <div className="color-add">
-            <h3 className="add-title">Добавить цвет</h3>
-            <div className="color-add-input">
-                <span>Название цвета</span>
-                <input
-                    placeholder='Введите название цвета...'
-                    value={colorName}
-                    onChange={onColorNameChange}
-                />
-            </div>
-            <div className="color-add-input">
-                <span>HEX-код</span>
-                <input
-                    placeholder='Введите hex-код цвета...'
-                    value={colorHex}
-                    onChange={onColorHexChange}
-                />
-            </div>
-            <button
-                onClick={() => {
-                    fetchData({ id: 0, name: colorName, hex: colorHex })
-                }}
-            >
-                Добавить цвет</button>
-        </div>
-    )
-}
+  return (
+    <div className="color-add">
+      <h3 className="add-title">Добавить цвет</h3>
+      <div className="color-add-input">
+        <span>Название цвета</span>
+        <input
+          placeholder="Введите название цвета..."
+          value={colorName}
+          onChange={onColorNameChange}
+        />
+      </div>
+      <div className="color-add-input">
+        <span>HEX-код</span>
+        <input
+          placeholder="Введите hex-код цвета..."
+          value={colorHex}
+          onChange={onColorHexChange}
+        />
+      </div>
+      <button
+        onClick={() => {
+          fetchData({ id: 0, name: colorName, hex: colorHex });
+        }}
+      >
+        Добавить цвет
+      </button>
+    </div>
+  );
+};
